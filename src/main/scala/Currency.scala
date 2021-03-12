a4f804f2446d82d7752b46b150c85f1603fe2ae7
hhimanshu <harit.himanshu@yahoo.in>
2021-03-11 17:03:45 -0800

    Refactor Companion Object

--- src/main/scala/Currency.scala
+++ src/main/scala/Currency.scala
@@ -1,26 +1,25 @@
 object Currency {
   private val conversionTable: Map[String, Double] = Map("CAD" -> 1 / 1.30, "NZD" -> 1 / 1.50)
-  def apply(code: String, amount: Double) = new Currency(code, amount)
-  def createUSD(amount: Double) = new Currency("USD", amount)
-  def createNZD(amount: Double) = new Currency("NZD", amount)
-  def createCAD(amount: Double) = new Currency("CAD", amount)
-}
-class Currency(code: String, amount: Double) {
-  import Currency.conversionTable
-  private val _code = code
-  private val _amount = amount
-  private val _inUSD: Double = getCostInUsd
-
-  private def getCostInUsd: Double = {
-    _code match {
-      case "USD" => _amount
-      case "NZD" => _amount * conversionTable("NZD")
-      case "CAD" => _amount * conversionTable("CAD")
-      case _ => throw new IllegalArgumentException(s"No conversion available for ${_code}")
+  private def getCostInUsd(code: String, amount: Double): Double = {
+    code match {
+      case "USD" => amount
+      case "NZD" => amount * conversionTable("NZD")
+      case "CAD" => amount * conversionTable("CAD")
+      case _ => throw new IllegalArgumentException(s"No conversion available for ${code}")
     }
   }
+  def apply(code: String, amount: Double) = new Currency(code, amount, getCostInUsd(code, amount))
+  def createUSD(amount: Double): Currency = Currency("USD", amount)
+  def createNZD(amount: Double): Currency = Currency("NZD", amount)
+  def createCAD(amount: Double): Currency = Currency("CAD", amount)
+}
 
-  override def toString: String = s"${code} ${_amount} = USD ${_inUSD}"
+class Currency(code: String, amount: Double, inUSD: Double) {
+  private val _code = code
+  private val _amount = amount
+  private val _inUSD: Double = inUSD
+
+  override def toString: String = s"${_code} ${_amount} = USD ${_inUSD}"
 }
 
 object CurrencyRunner extends App {
